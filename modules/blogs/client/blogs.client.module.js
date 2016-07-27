f51584538e727c0ab6af72b070c07aba6a34aa79
Blake Nicholson <blaken2021@gmail.com>
2016-07-27 18:13:29 -0400

    added new styles and removed blog module

--- modules/blogs/client/blogs.client.module.js
+++
@@ -1,7 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('blog');// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('blog.services');
-  app.registerModule('blog.routes', ['ui.router', 'core.routes', 'blog.services']);
-}(ApplicationConfiguration));
